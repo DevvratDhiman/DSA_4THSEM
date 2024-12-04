b954ed84276d738fd45feda52960950310627723
DevvratDhiman <dhimandevvrat8@gmail.com>
2024-12-04 18:43:02 +0530

    dsa start

---
+++ BASICS/01program.java
@@ -0,0 +1,11 @@
+//traversing an array
+package basics; 
+class 01program {
+    public static void main(string[] args){
+        int []arr={10,20,30,40,50};
+        //traverse each index of an array 
+        for(int i=0;i<arr.length;i++){
+            System.out.println("element at index "+i+" is"+arr[i]);
+        }
+    }
+}
